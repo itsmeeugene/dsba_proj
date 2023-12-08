d75641ff5541ab9bb93ad9a72c556a149a305f24
Evgeny Pavlov <eapavlov_2@edu.hse.ru>
2023-12-08 21:20:53 +0300

    405 fixing

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pandas as pd
+import logging
 
 app = Flask(__name__)
+app.logger.setLevel(logging.DEBUG)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        start = int(request.form['start'])
+        end = int(request.form['end'])
+        df = pd.read_csv("car.csv")
+        avg = df[start:end]['selling_price'].mean()
+        return render_template("index.html", result=avg)
+    return render_template("index.html", result='')
 
 
 @app.route('/plots')
