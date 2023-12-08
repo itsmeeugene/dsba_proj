39dd9265bd78c178b8f1926c7529b29964ce64ea
Evgeny Pavlov <eapavlov_2@edu.hse.ru>
2023-12-08 21:34:48 +0300

    405 fixed

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
-import logging
 
 app = Flask(__name__)
-app.logger.setLevel(logging.DEBUG)
 
 
 @app.route('/', methods=['POST', 'GET'])
